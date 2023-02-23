9d37d80d5ba01a8d4ab5af6e9d5371317a009574
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-23 12:17:55 +0100

    test: add new test

--- src/article/article.service.spec.ts
+++ src/article/article.service.spec.ts
@@ -1,15 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleService } from './article.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ArticleService', () => {
   let service: ArticleService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticleService],
+      providers: [ArticleService, PrismaService],
     }).compile();
 
     service = module.get<ArticleService>(ArticleService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(async () => {
+    await PrismaService.$disconnect();
+  });
+
+  describe('create', () => {
+    it('should create an article', async () => {
+      const data = {
+        titre: 'Test article',
+        date_creation: new Date(),
+        date_suppression: new Date(),
+        url_img: 'https://example.com/image.jpg',
+        img_description: 'An example image',
+        img_titre: 'Example image',
+        sous_titre: 'Test subtitle',
+        contenu: 'Test content',
+        description: 'Test description',
+        meta_title: 'Test meta title',
+        categories: 'Test category',
+        url_video: 'https://example.com/video.mp4',
+        slug: 'test-article',
+        tags: 'Test tag',
+        status: 'published',
+      };
+      const createdArticle = await service.create(data);
+      expect(createdArticle).toMatchObject(data);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of articles', async () => {
+      const articles = await service.findAll();
+      expect(Array.isArray(articles)).toBe(true);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an article by id', async () => {
+      const article = await service.findOne(1);
+
+      expect(article.id).toBe(1);
+    });
   });
 
   it('should be defined', () => {
