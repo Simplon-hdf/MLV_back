73c50f6c5d43444e6af695ef60c1d41a36cab3dd
nathancuvelier <nathancuvelier59@icloud.com>
2023-02-23 13:13:39 +0100

    feat: add auth ressources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UtilisateursModule } from './utilisateurs/utilisateurs.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UtilisateursModule, PagesModule],
+  imports: [UtilisateursModule, ArticleModule,PagesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
