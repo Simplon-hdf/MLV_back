4248dcdbfaba99f2f6d8f9cbd5da6a4cd422ace8
NathanCvl <nathancuvelier59@icloud.com>
2023-02-24 11:36:27 +0100

    fix: fix some error in auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/guard/local-auth.guard';
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -13,9 +14,10 @@ export class AppController {
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
+    console.log(req.user);
     return req.user;
   }
 }
