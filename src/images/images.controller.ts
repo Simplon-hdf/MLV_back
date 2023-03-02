abe61bf058e42e86ac84b67bb8b9943e71a24427
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-02 16:41:24 +0100

    feat: add ApiConsumes in images.controller

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -19,16 +19,29 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiConsumes, ApiTags, ApiBody } from '@nestjs/swagger';
 
 @Controller('images')
 @ApiTags('Images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
-
   // récupere une image, la redimensionne, la compresse puis la sauvegarde
   // dans le dossier public/images.
-  @Post('upload:path')
+  @Post('upload')
+  @ApiBody({
+    required: true,
+    type: 'multipart/form-data',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiConsumes('multipart/form-data')
   @UseInterceptors(
     FileInterceptor('image', {
       storage: diskStorage({
@@ -54,7 +67,7 @@ export class ImagesController {
   )
   async uploadImage(@UploadedFile() file) {
     // Resizing image to 300x300 using sharp module
-    this.imagesService.compressImage(file, 'jpeg'); // Do something with the image (e.g. save it to the database, etc.)
+    this.imagesService.compressImage(file, 'jpg'); // Do something with the image (e.g. save it to the database, etc.)
 
     // Return the image file name and path
     return {
