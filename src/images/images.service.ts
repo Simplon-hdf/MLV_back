c39dea9f9e4f8ecc683c16951b4b88d051d6558a
delannoy kurt <delannoykurt666@gmail.com>
2023-03-03 19:30:41 +0100

    fix: resolved conflit image.service

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -3,31 +3,39 @@ import { CreateImageDto } from './dto/create-image.dto';
 import { createWriteStream } from 'fs';
 import * as sharp from 'sharp';
 import * as path from 'path';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 
 @Injectable()
 export class ImagesService {
   // compress images
   async compressImage(file: Express.Multer.File, format: string = 'jpg') {
     console.log('compress call debug');
+    const baseDir = './res/public/images/';
+    const filePath = path.join(baseDir, file.filename);
     const compressedImage = await sharp(file.path)
       .resize({ width: 200, height: 200 })
       .jpeg({ quality: 80 }) // compress JPEG images
       .png({ quality: 80 }) // compress PNG images
       .gif({ quality: 80 }) // compress GIF images
-      .pdf({ quality: 60 }) // compress PDF images
-      //.toBuffer()
       .toFormat(format)
-      .toFile('../res/public/images/' + file.filename, (err, info) => {
-        if (err) {
-          throw new BadRequestException(err);
-        }
-      }); // externaliser le chemin.
-    return sharp(compressedImage);
-  }
+      .toBuffer();
 
-  remove(filename) {
-    fs.unlink('../res/public/images/' + filename); // externaliser le chemin.
-    console.log('delete call debug -> service');
+    const writeStream = createWriteStream(filePath);
+    writeStream.write(compressedImage);
+    return compressedImage;
+  }
+  private readonly imagePath = './res/public/images/';
+  async remove(filename: string): Promise<string> {
+    const filePath = path.join(this.imagePath, filename);
+
+    return new Promise<string>((resolve, reject) => {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Error deleting file: ${filePath}`, err);
+          return reject(err);
+        }
+        resolve(filename);
+      });
+    });
   }
 }
