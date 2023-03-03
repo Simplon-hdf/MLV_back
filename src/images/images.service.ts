79803638f2a237092800668148d598f633a4206c
delannoy kurt <delannoykurt666@gmail.com>
2023-03-03 15:24:36 +0100

    fix: image.service and image.controller

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -7,54 +7,27 @@ import * as fs from 'fs/promises';
 
 @Injectable()
 export class ImagesService {
-  async compressAndSaveImage(
-    file: Express.Multer.File,
-    formats: string[] = ['jpg', 'webp', 'png', 'svg'],
-  ) {
-    // taille de l'image
-    const MAX_SIZE = 1024 * 1024; // 1 MB
-    if (file.size > MAX_SIZE) {
-      throw new BadRequestException(
-        `Image too large. Max size ${MAX_SIZE / 1024} KB`,
-      );
-    }
-    // image compressé
-    const compressedImages = await Promise.all(
-      formats.map(async (format) => {
-        const compressedImageBuffer = await sharp(file.buffer)
-          .resize({ with: 500 })
-          .toFormat(format)
-          .toBuffer();
-        const fileName = `${Date.now()}-${file.originalname}.${format}`;
-        const filePath = path.join(
-          __dirname,
-          '..',
-          'res',
-          'public',
-          'images',
-          fileName,
-        );
-        await fs.writeFile(filePath, compressedImageBuffer);
-        return { url: `/res/public/images/${fileName}` };
-      }),
-    );
-    return compressedImages;
-  }
-
   // compress images
-  async compressImage(file: Express.Multer.File, format: string = 'jpeg') {
-    const compressedImage = await sharp(file.buffer)
-      .resize(300, 300)
+  async compressImage(file: Express.Multer.File, format: string = 'jpg') {
+    console.log('compress call debug');
+    const compressedImage = await sharp(file.path)
+      .resize({ width: 200, height: 200 })
+      .jpeg({ quality: 80 }) // compress JPEG images
+      .png({ quality: 80 }) // compress PNG images
+      .gif({ quality: 80 }) // compress GIF images
+      .pdf({ quality: 60 }) // compress PDF images
+      //.toBuffer()
       .toFormat(format)
-      .jpeg({ quality: 75 }) // compress JPEG images
-      .png({ quality: 60 }) // compress PNG images
-      .gif({ quality: 60 }) // compress GIF images
-      .svg({ quality: 60 }) // compress SVG images
-      .toBuffer();
-    return compressedImage;
+      .toFile('../res/public/images/' + file.filename, (err, info) => {
+        if (err) {
+          throw new BadRequestException(err);
+        }
+      }); // externaliser le chemin.
+    return sharp(compressedImage);
   }
 
   remove(filename) {
     fs.unlink('../res/public/images/' + filename); // externaliser le chemin.
+    console.log('delete call debug -> service');
   }
 }
