01181bac8e83a48b724ac2c2164fa6dce25dbcf3
Josue <96113041+Rowada@users.noreply.github.com>
2023-03-09 21:20:23 +0100

    feat: add alert controller

--- src/alert/alert.controller.ts
+++ src/alert/alert.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { AlertService } from './alert.service';
 import { CreateAlertDto } from './dto/create-alert.dto';
 import { UpdateAlertDto } from './dto/update-alert.dto';
-
+import { alert } from '@prisma/client';
 @Controller('alert')
 export class AlertController {
   constructor(private readonly alertService: AlertService) {}
 
   @Post()
-  create(@Body() createAlertDto: CreateAlertDto) {
-    return this.alertService.create(createAlertDto);
+  create(@Body() data: alert): Promise<alert> {
+    return this.alertService.create(data);
   }
 
-  @Get()
+  @Get('alerts')
   findAll() {
     return this.alertService.findAll();
   }
@@ -20,11 +20,6 @@ export class AlertController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.alertService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlertDto: UpdateAlertDto) {
-    return this.alertService.update(+id, updateAlertDto);
   }
 
   @Delete(':id')
