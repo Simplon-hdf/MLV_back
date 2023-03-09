98e1bbc7cd78d90e9aaa63a6a364eabbb3f95cf0
Josue <96113041+Rowada@users.noreply.github.com>
2023-03-09 20:52:50 +0100

    feat: add alert resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MessagesModule } from './messages/messages.module';
 import { ImagesModule } from './images/images.module';
+import { AlertModule } from './alert/alert.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { ImagesModule } from './images/images.module';
     MailModule,
     MessagesModule,
     ImagesModule,
+    AlertModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
