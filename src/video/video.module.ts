720cffded10d366d8af55b6ab819ccfb9f8065f3
Elodie mongellaz <mongellaz.e@gmail.com>
2023-04-11 14:11:24 +0200

    feat: add import jwtModule

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
+import { RoleGuard } from 'src/auth/role/role.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [VideoController],
-  providers: [VideoService],
+  providers: [VideoService, RoleGuard],
 })
 export class VideoModule {}
