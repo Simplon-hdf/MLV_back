942a614ec5369a89d7a3044c68b1a7cb7dc26a26
Elodie mongellaz <mongellaz.e@gmail.com>
2023-04-14 12:32:44 +0200

    feat: add method uploadVideo to compress videos

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   Query,
   UseInterceptors,
+  UseGuards,
+  UploadedFile,
 } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { CreateVideoDto } from './dto/create-video.dto';
@@ -19,15 +21,19 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { RoleGuard } from 'src/auth/role/role.guard';
 
 @Controller('video')
 @ApiTags('Video')
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
 
-  @Post()
-  @ApiQuery({ name: 'role', enum: RolesEnum })
+  // @UseGuards(JwtAuthGuard, RoleGuard)
   @Roles(RolesEnum.conseiller, RolesEnum.moderateur, RolesEnum.administrateur)
+  @Post('upload')
+  @ApiQuery({ name: 'role', enum: RolesEnum })
   @ApiBody({
     required: true,
     type: 'multipart/form-data',
@@ -49,7 +55,7 @@ export class VideoController {
         filename: (req, file, cb) => {
           const randomName = uuidv4();
           console.log('debug filename', randomName, extname(file.originalname));
-          cb(null, `${randomName}$extname(file.originalname)`);
+          cb(null, `${randomName}${path.extname(file.originalname)}`);
         },
       }),
       fileFilter: (req, file, cb) => {
@@ -59,34 +65,14 @@ export class VideoController {
         cb(null, true);
       },
       limits: {
-        fileSize: 5 * 1024 * 1024, // limit to 5 Mo
+        fileSize: 500 * 1024 * 1024, // limit to 500 Mo
       },
     }),
   )
-  async create(
-    @Body() createVideoDto: CreateVideoDto,
-    @Query('role') role: RolesEnum = RolesEnum.administrateur,
-  ) {
-    return this.videoService.create(createVideoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.videoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.videoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVideoDto: UpdateVideoDto) {
-    return this.videoService.update(+id, updateVideoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.videoService.remove(+id);
+  async uploadVideo(@UploadedFile() file) {
+    await this.videoService.compressVideo(file, 'mp4');
+    return {
+      compressedFilename: `${file.filename}`,
+    };
   }
 }
