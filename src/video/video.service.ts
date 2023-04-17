16f34f5ff4fe779ba22ef7a748c6c660a0347b57
Elodie mongellaz <mongellaz.e@gmail.com>
2023-04-17 11:31:24 +0200

    feat: add delete video in service

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import * as ffmpeg from 'fluent-ffmpeg';
 import * as fs from 'fs';
+import * as path from 'path';
 import { ArticleService } from 'src/article/article.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -18,6 +19,10 @@ export class VideoService {
     file: Express.Multer.File,
     format = 'mp4',
   ): Promise<void> {
+    const outputFilePath = path.join(
+      this.videoPath,
+      `compressed_${file.filename}`,
+    );
     return new Promise<void>((resolve, reject) => {
       ffmpeg(file.path)
         .videoCodec('libx264')
@@ -31,10 +36,34 @@ export class VideoService {
         })
         .on('end', function () {
           console.log(`Transcoding of succeeded!`);
-          fs.unlinkSync(file.path);
+          setTimeout(() => {
+            fs.unlinkSync(file.path);
+          }, 500);
           resolve();
         })
-        .save(this.videoPath);
+        .save(outputFilePath);
+    });
+  }
+
+  // remove video on server
+  async remove(filename: string): Promise<string> {
+    const filePath = `./res/public/videos/${filename}`;
+    // if (!fs.existsSync(filePath)) {
+    //   console.error(`Error deleting file: ${filePath}. File not found.`);
+    //   throw new InternalServerErrorException();
+    // }
+
+    // Delete video
+    return new Promise<string>((resolve, reject) => {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Error deleting file: ${filePath}`, err);
+          reject(err);
+        } else {
+          console.log(`File ${filePath} deleted successfully.`);
+          resolve(filename);
+        }
+      });
     });
   }
 }
